72d03395a2371587828aef1f9548238a5148e9ad
Pokey Rule <pokey.rule@gmail.com>
2017-08-29 14:29:36 +0100

    Some cleanup

--- investing/cli.py
+++ investing/cli.py
@@ -3,6 +3,7 @@
 """Console script for investing."""
 
 import locale
+from functools import partial
 
 import click
 import pandas as pd
@@ -20,44 +21,56 @@ def to_numeric(s):
 )
 def main(unrealized):
     """Console script for investing."""
+    # Locale formatting init
     locale.setlocale(locale.LC_ALL, 'en_US')
+    format_currency = partial(locale.currency, grouping=True)
 
+    # Import csv
     df = pd.read_csv(unrealized)
 
+    # Convert to numeric
     df['gain_loss_abs'] = to_numeric(df['Gain/Loss($)'])
     df['gain_loss_pct'] = to_numeric(df['Gain/Loss(%)'])
     df['market_value'] = to_numeric(df['Market Value'])
 
+    # Format
+    df['Market Value'] = df.market_value.apply(format_currency)
+    df['Gain/Loss($)'] = df.gain_loss_abs.apply(format_currency)
+
+    # Total holdings
     total = df.market_value.sum()
 
+    # Sell stocks with lowest gain/loss % until we start to accrue a gain
     df.sort_values('gain_loss_pct', inplace=True)
-    df['cumsum'] = df.gain_loss_abs.cumsum(axis=0)
-    df['sell'] = df['cumsum'] < 0
+    df['sell'] = df.gain_loss_abs.cumsum() < 0
     groups = df.groupby('sell')
+    to_sell = pd.DataFrame(groups.get_group(True))
 
+    # Summarize stocks sold / kept
     results = pd.DataFrame(dict(
         count=groups.market_value.count(),
-        sum=groups.market_value.sum(),
-        pct=groups.market_value.sum() / total,
+        sum=groups.market_value.sum().apply(format_currency),
+        pct=(100 * groups.market_value.sum() / total).map('{:,.2f}%'.format),
     ), columns=['count', 'sum', 'pct'])
 
-    click.echo('Total money: {}'.format(
-        locale.currency(total, grouping=True)
+    # Print info
+    click.echo('Total money: {}'.format(format_currency(total)))
+    click.echo('Total stocks: {}\n'.format(df.market_value.count()))
+    click.echo('Sell/keep:\n{}\n'.format(results))
+    click.echo('Realized gain/loss: {}\n'.format(
+        format_currency(to_sell.gain_loss_abs.sum())
     ))
-    click.echo('Total stocks: {}'.format(df.market_value.count()))
-    click.echo()
-
-    click.echo('Sell/keep:\n{}'.format(results))
-    click.echo()
-
-    loss_to_harvest = groups.get_group(True).gain_loss_abs.sum()
-    click.echo('Loss left to harvest: {}'.format(
-        locale.currency(loss_to_harvest, grouping=True)
-    ))
-    to_sell = pd.DataFrame(groups.get_group(True))
     click.echo('Stocks to sell:\n{}'.format(
         to_sell[['Symbol', 'Name', 'Market Value', 'Gain/Loss($)']]
     ))
+
+    # Output csv
+    del df['gain_loss_abs']
+    del df['gain_loss_pct']
+    del df['market_value']
+    del df['Day Change($)']
+    del df['Day Change(%)']
+    df.to_csv('stocks.csv', index=False)
 
 
 if __name__ == "__main__":
